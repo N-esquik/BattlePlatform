3a1efbd5697433536239932bbae89dcad9adb3e8
N-esquik <136918172+N-esquik@users.noreply.github.com>
2025-04-08 03:37:56 +0300

    Delete Scripts directory

--- Scripts/Player/Attack/Vampirism/SpawnerAura.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SpawnerAura : MonoBehaviour
-{
-    public GameObject Create(GameObject prefab,Transform transform)
-    {
-        return Instantiate(prefab, transform.position, Quaternion.identity);
-    }
-}
